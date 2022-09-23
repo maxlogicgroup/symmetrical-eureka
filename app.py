9a4d4cfc23676b62dd15f03901b16e2217806e73
christiaanlok <61160959+christiaanlok@users.noreply.github.com>
2022-09-23 21:48:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
-from dash import Dash, dcc, html
-import plotly.express as px
-import pandas as pd
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8080/ in your web browser.
 
+from dash import Dash, html, dcc
 
 app = Dash(__name__)
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
-
 app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
-])
+    html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
 
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
+                     ['Montréal', 'San Francisco'],
+                     multi=True),
+
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+    ], style={'padding': 10, 'flex': 1}),
+
+    html.Div(children=[
+        html.Label('Checkboxes'),
+        dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
+                      ['Montréal', 'San Francisco']
+        ),
+
+        html.Br(),
+        html.Label('Text Input'),
+        dcc.Input(value='MTL', type='text'),
+
+        html.Br(),
+        html.Label('Slider'),
+        dcc.Slider(
+            min=0,
+            max=9,
+            marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
+            value=5,
+        ),
+    ], style={'padding': 10, 'flex': 1})
+], style={'display': 'flex', 'flex-direction': 'row'})
 
 if __name__ == '__main__':
      app.run_server(host="0.0.0.0", debug=True, port=8080)
 
+#if __name__ == '__main__':
+#     app.run_server(host="127.0.0.1", debug=True, port=8080)
 #TODO
 #if __name__ == "__main__":
 #    from waitress import serve
